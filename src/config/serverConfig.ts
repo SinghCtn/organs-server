a38e1e71c809717d5ff0e0c5e91444a64c256433
Chetan Singh <chetansingh2k1@gmail.com>
2024-12-15 23:23:19 +0530

    tr

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173/" }));
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req: Request, res: Response) => {
   res.setHeader("Content-Type", "text/plain");
